a36a685db095fed90a642e8b1c4034848dbc91c0
Matias Garay <matiasgarayp16@gmail.com>
2022-08-08 17:45:46 -0300

    3er prueba

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -28,10 +28,7 @@ const nuevoModulo = 21 % 5 === 1;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
-  function devolverString(str){
-    console.log(str)
-  }
-  devolverString('Hola' )
+  return (str);
 }
 
 function suma(x, y) {
